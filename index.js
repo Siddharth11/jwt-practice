8bee858ab5e03b9903b37fdd643a74b5fa71c655
siddharth11 <siddharth2parmar@gmail.com>
2019-07-27 15:38:08 +0200

    Add express and create a basic server

---
+++ index.js
@@ -0,0 +1,18 @@
+const express = require('express');
+
+const app = express();
+const PORT = 8888;
+
+app.get('/status', (req, res) => {
+  const localTime = new Date().toLocaleTimeString();
+
+  res.status(200).send(`Server time: ${localTime}`);
+});
+
+app.get('*', (req, res) => {
+  res.sendStatus(404);
+});
+
+app.listen(PORT, () => {
+  console.log(`Server running on: http://127.0.0.1:${PORT}`);
+});
