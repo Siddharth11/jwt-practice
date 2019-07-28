aecb94cc233f2e6c7c4a0a702de0ca621ea52c1f
siddharth11 <siddharth2parmar@gmail.com>
2019-07-28 14:32:47 +0200

    Setup basic authentication and send signed token with 200

--- index.js
+++ index.js
@@ -1,17 +1,47 @@
 const express = require('express');
 const bodyParser = require('body-parser');
+const jwt = require('jsonwebtoken');
 
 const app = express();
 const PORT = process.env.PORT || 8888;
+const users = [
+  { id: 1, user: 'admin', password: 'admin' },
+  { id: 2, user: 'guest', password: 'guest' },
+];
 
 app.use(bodyParser.json());
 
 app.post('/login', (req, res) => {
-  const user = req.body.username;
+  const { username, password } = req.body;
+
+  if (!username || !password) {
+    res
+      .status(400)
+      .send('Username and password are required');
+    return;
+  }
+
+  const user = users.find(u => {
+    return u.user === username && u.password === password;
+  });
+
+  if (!user) {
+    res
+      .status(401)
+      .send('User not found');
+    return;
+  }
+
+  const secret = 'my_secret_key';
+  const tokenExpiry = '3 hours';
+  const token = jwt.sign({
+    sub: user.id,
+    username,
+  }, secret, { expiresIn: tokenExpiry });
 
   res
     .status(200)
-    .send(`Logged in with user: ${user}`);
+    .send({ access_token: token });
 });
 
 app.get('/status', (req, res) => {
